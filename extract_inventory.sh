0e802163094ba763f9e42a82c8a7ed4c29b9b1b5
박재호 (Jaeho Park) <jaeho.p@hanwha.com>
2024-12-06 20:43:43 +0900

    Update extract_inventory.sh

--- extract_inventory.sh
+++ extract_inventory.sh
@@ -46,7 +46,7 @@ sso_registration_scopes = sso:account:access
 EOT
     echo ""
     # AWS SSO Session Login
-    aws sso login --sso-session "$SSO_SESSION_NAME"
+    aws sso login --sso-session "$SSO_SESSION_NAME" --no-browser
 
     echo "\n\033[1;34mSelect AWS Account Profile Config Setup Method.\033[0m"
     echo "\033[1;34m(1) Auto: The region for all AWS account profiles is set to Same Region(Default Region).\033[0m"
